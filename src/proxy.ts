6c7a72480c43eb506f6b063f929e07563eb0f9e4
thangvaduc5 <thangminhhoang7112002@gmail.com>
2026-03-02 22:25:11 +0700

    fix conflic version

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,26 +1,23 @@
 import { NextRequest, NextResponse } from "next/server";
 import { jwtVerify } from "jose";
-import {
-  USER_PROTECTED,
-  ADMIN_PROTECTED,
-  AUTH_ROUTES,
-} from "@/routes"
+import { USER_PROTECTED, ADMIN_PROTECTED, AUTH_ROUTES } from "@/routes";
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-export async function middleware(req: NextRequest) {
+export async function proxy(req: NextRequest) {
   const url = req.nextUrl.clone();
   const isAuthRoute = AUTH_ROUTES.includes(url.pathname);
   const token = req.cookies.get("session")?.value;
   console.log(token);
   if (!token) {
     // === CASE 1: Không có token -> chỉ chặn các route cần login ===
-    const isUserProtected = USER_PROTECTED.some((r) => url.pathname.startsWith(r));
-    const isAdminProtected = ADMIN_PROTECTED.some((r) => url.pathname.startsWith(r));
+    const isUserProtected = USER_PROTECTED.some((r) =>
+      url.pathname.startsWith(r),
+    );
+    const isAdminProtected = ADMIN_PROTECTED.some((r) =>
+      url.pathname.startsWith(r),
+    );
 
-    if (
-      isUserProtected ||
-      isAdminProtected
-    ) {
+    if (isUserProtected || isAdminProtected) {
       return NextResponse.redirect(new URL("/", req.url));
     }
 
@@ -36,19 +33,22 @@ export async function middleware(req: NextRequest) {
     const { payload } = await jwtVerify(token, secret);
 
     // ✅ Nếu đã đăng nhập mà vào /login hoặc /register => redirect về "/"
-    if(isAuthRoute) {
+    if (isAuthRoute) {
       return NextResponse.redirect(new URL("/", req.url));
     }
 
     // ✅ Nếu là admin mới được vào /admin-dashboard
-    if (ADMIN_PROTECTED.some((r) => url.pathname.startsWith(r)) && payload.role !== "admin") {
+    if (
+      ADMIN_PROTECTED.some((r) => url.pathname.startsWith(r)) &&
+      payload.role !== "admin"
+    ) {
       return NextResponse.redirect(new URL("/", req.url));
     }
 
     return NextResponse.next();
   } catch (err) {
     console.error("JWT error:", err);
-    
+
     const response = NextResponse.redirect(new URL("/login", req.url));
     // ✅ Xóa session cookie
     response.cookies.delete("session");
@@ -57,10 +57,10 @@ export async function middleware(req: NextRequest) {
 }
 
 export const config = {
-    matcher: [
+  matcher: [
     // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
     // Always run for API routes
     // '/(api|trpc)(.*)',
   ],
-}
+};
